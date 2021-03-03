62d2b7ee6fd157a1e8a88d3e5bd2948d15650170
Leonardo Nardy <nardyleo@hotmail.com>
2021-03-03 12:49:36 -0300

    Primeiro commit, inclusao do plugin capacitor e build android ios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'detalhes',
+    loadChildren: () => import('./pages/detalhes/detalhes.module').then( m => m.DetalhesPageModule)
+  },
 ];
 
 @NgModule({
